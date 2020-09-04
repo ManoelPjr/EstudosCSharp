98d420f7933cee9d84cbfc9078d279e54702b193
ManoelPjr <napolesjr@gmail.com>
2020-09-03 23:04:44 -0300

    Generics 2.1
    Com uso de generics e restrição que tem que ser comparaveis ou seja o tipo generico tem que implementar a interface ICompareble

--- Course/GenericsCS2/services/CalculationService.cs
+++ Course/GenericsCS2/services/CalculationService.cs
@@ -6,17 +6,17 @@ namespace GenericsCS2
 {
     class CalculationService
     {
-        public int Max(List<int> list)
+        public T Max<T>(List<T> list) where T :IComparable
         {
             if (list.Count == 0)
             {
                 throw new InvalidOperationException("List is empty");
             }
 
-            int max = list[0];
+            T max = list[0];
             for (int i = 1; i < list.Count; i++)
             {
-                if (list[i]>max)
+                if (list[i].CompareTo(max)>0)
                 {
                     max = list[i];
                 }
