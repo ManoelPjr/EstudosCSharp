bddb5f25899e140fd9347c8b9a2e520e311d61b7
ManoelPjr <napolesjr@gmail.com>
2020-09-03 22:49:13 -0300

    Generics 2
    Ainda sem uso de generics

---
+++ Course/GenericsCS2/services/CalculationService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsCS2
+{
+    class CalculationService
+    {
+        public int Max(List<int> list)
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException("List is empty");
+            }
+
+            int max = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i]>max)
+                {
+                    max = list[i];
+                }
+            }
+            return max;
+        }
+    }
+}
